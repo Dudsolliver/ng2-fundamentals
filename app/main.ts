e510592713e53949ae85b683a99ceed960d929bd
Eduardo Oliveira Almeida <dudsolliver@gmail.com>
2017-09-10 15:44:29 -0300

    Setting up the first component

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import { plataformBrowserDynamic } from '@angular/core/plataform-browser-dynamic'
+
+import { AppModule } from './app.module'
+
+
+plataformBrowserDynamic().bootstrapModule(AppModule)
